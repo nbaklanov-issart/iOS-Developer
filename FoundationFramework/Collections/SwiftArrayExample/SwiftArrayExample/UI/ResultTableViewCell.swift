e8716ab68594cb8d50ed71bfe27e65535bccc7c2
Nikolai Baklanov <nbaklanov@issart.com>
2017-10-09 08:39:28 +0600

    Изменил структуру. Добавил пример массивов для swift

---
+++ FoundationFramework/Collections/SwiftArrayExample/SwiftArrayExample/UI/ResultTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ResultTableViewCell.swift
+//  SwiftArrayExample
+//
+//  Created by Nikolai Baklanov on 07/10/2017.
+//  Copyright © 2017 Nikolai. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var resultTitle:UILabel!;
+    @IBOutlet weak var resultValueTitle:UILabel!;
+    
+}
