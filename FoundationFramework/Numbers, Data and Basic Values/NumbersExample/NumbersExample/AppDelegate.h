040ccc11e08dcabf48a26151a22b84d2449e716e
Nikolai Baklanov <nbaklanov@issart.com>
2017-10-11 19:47:30 +0600

    Добавлен пример для базовой работы с числами

---
+++ FoundationFramework/Numbers, Data and Basic Values/NumbersExample/NumbersExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NumbersExample
+//
+//  Created by Nikolai Baklanov on 10/10/2017.
+//  Copyright © 2017 Nikolai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoundationFramework/Numbers, Data and Basic Values/NumbersExample/NumbersExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NumbersExample
+//
+//  Created by Nikolai Baklanov on 10/10/2017.
+//  Copyright © 2017 Nikolai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
