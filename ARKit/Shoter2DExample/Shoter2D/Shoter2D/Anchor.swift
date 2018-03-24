55be265194f994360b671b129a9bd193385f5d90
Nikolai Baklanov <nbaklanov@issart.com>
2018-03-24 14:20:19 +0600

    ARKit. Добавил проект для примера ARSpriteKit

---
+++ ARKit/Shoter2DExample/Shoter2D/Shoter2D/Anchor.swift
@@ -0,0 +1,14 @@
+//
+//  Anchor.swift
+//  Shoter2D
+//
+//  Created by Nikolai Baklanov on 24/03/2018.
+//  Copyright © 2018 Nikolai Baklanov. All rights reserved.
+//
+
+import ARKit
+
+class Anchor: ARAnchor
+{
+    var type: NodeType?
+}
