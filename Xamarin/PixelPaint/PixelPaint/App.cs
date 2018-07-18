ecba80db70d47ab7d584e1d9ac78be203f6ae850
Nikolai Baklanov <nbaklanov@issart.com>
2018-07-18 06:47:37 +0600

    Imported MVVMCross

---
+++ Xamarin/PixelPaint/PixelPaint/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MvvmCross.ViewModels;
+
+namespace PixelPaint
+{
+    public class App:MvxApplication
+    {
+        public override void Initialize()
+        {
+            //Mvx.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<SelectImageViewModel>();
+        }
+    }
+}
