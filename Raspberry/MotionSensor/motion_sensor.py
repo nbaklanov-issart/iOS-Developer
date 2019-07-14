7b96e437e864aeb42c3ce97a9659c77866e81f55
Nikolai <nbaklanov@issart.com>
2019-07-14 22:13:53 +0600

    Refactoring for Raspberry

--- Raspberry/MotionSensor/motion_sensor.py
+++ Raspberry/MotionSensor/motion_sensor.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 import os
-import datetime
 import sys
 import time
 import subprocess
@@ -13,26 +12,9 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
 while True:
-    i=GPIO.input(SENSOR_PIN)
-
-    if i==0:                 
-        print ("No intruders")    
+    signal = GPIO.input(SENSOR_PIN)
+    if signal == 0:
         time.sleep(0.1)
-    elif i==1:
-        print ("Intruder detected")
+    elif signal == 1:
         time.sleep(0.1)
-
-        # read the absolute path
-        script_dir = os.path.dirname(__file__)
-        # call the .sh to capture the image
         os.system('/home/pi//webcam.sh')
-        #get the date and time, set the date and time as a filename.
-        currentdate = datetime.datetime.now().strftime("%Y-%m-%d_%H%M")
-        # create the real path
-        rel_path = currentdate +".jpg"
-        #  join the absolute path and created file name
-        abs_file_path = os.path.join(script_dir, rel_path)
-        print(abs_file_path)
-
-        
-
