6f95799e758e67033969b0cabb6ed09de3b34bbf
Nikolay Baklanov <>
2019-10-24 11:11:45 +0600

    Flutter first task

--- Udemy/Flutter Course/flutter_assignment/lib/main.dart
+++ Udemy/Flutter Course/flutter_assignment/lib/main.dart
@@ -1,30 +1,70 @@
-// 1) Create a new Flutter App (in this project) and output an AppBar and some text
-// below it
-// 2) Add a button which changes the text (to any other text of your choice)
-// 3) Split the app into three widgets: App, TextControl & Text
-
+import 'dart:developer';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {    
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  String _stateText = "Some Text From State";
+
+  void changeText() {
+    setState(() {      
+      _stateText = "Button Pressed";
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
+    log(_stateText) ; 
+    return MaterialApp ( 
+      home: Scaffold(
             appBar: AppBar(title: Text("My First assignment")),
             body: Column( 
               children: <Widget>[
-                Text("This is default text"),
-                RaisedButton(
-                  child: Text("My Button"),
-                  onPressed: () => {                    
-                    setState(() {
-                      _counter++;
-                    });
-                  },
-                )
+                TextControl(_stateText),
+                MyRaisedButton(changeText)
               ],
-            )      
+            )
+      )      
     );
   }
+}
+
+
+
+class TextControl extends StatelessWidget {
+  String _controlText = "Default Control Text";
+
+  TextControl(this._controlText);
+
+  @override
+  Widget build(BuildContext context) {    
+    return Column(
+      children: <Widget>[
+        Text(_controlText)
+      ],
+    );
+  }
+}
+
+
+class MyRaisedButton extends StatelessWidget {
+  Function _clickFunction;
+
+  MyRaisedButton(this._clickFunction);
+  
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      child: Text("Custom Button Text"),
+      onPressed: _clickFunction,
+    );
+  }
+
 }
